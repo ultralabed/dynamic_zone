7f600e8222b522746e685e7266cc4380ef68eff9
ed <ultralabed@gmail.com>
2016-03-05 17:09:02 +0800

    add config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,6 +18,7 @@ set :branch, "master"
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
+role :app, %w{root@45.32.58.57}, my_property: :my_value
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
